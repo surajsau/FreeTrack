c0c8e31298bec6ca2075566b8997550dbed68805
surajsau <suraj.sau@snapdeal.com>
2016-07-20 13:46:11 +0530

    first commit

---
+++ app/src/main/java/com/halfplatepoha/freetrack/ConsumerMapActivity.java
@@ -0,0 +1,13 @@
+package com.halfplatepoha.freetrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsumerMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumer_map);
+    }
+}
